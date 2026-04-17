9df3a98f0caa02c1e51029a37e395fbd8deb3b2f
Rachel Shaw <rachel.shaw@yale.edu>
2026-04-17 10:14:02 -0400

    adding references

--- test.R
+++ test.R
@@ -1,3 +1,16 @@
+#setting up Github#
+
+##REFERENCES##
+#main reference tutorial: https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
+    #https://happygitwithr.com/new-github-first#new-github-first
+#how git/github works: https://r-bio.github.io/intro-git-rstudio/
+#nice workflow of how git works: https://www.git-tower.com/blog/workflow-of-git
+#other setup refs: https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup, 
+    #https://happygitwithr.com/install-git.html,
+    #https://www.geeksforgeeks.org/r-language/link-your-github-account-with-r-studio/,
+    #https://docs.posit.co/ide/user/ide/guide/tools/version-control.html
+
+
 #testing github  
 #making a token
 library(usethis)
